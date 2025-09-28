d69a9edc534cad76b1df37cca2afc05c814b5e4e
Kavz44 <kavingregary@outlook.com>
2025-09-28 13:45:35 +0800

    simple dummy UI for the app

--- FirstAPICallApp/ContentView.swift
+++ FirstAPICallApp/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing:20) {
+            Circle()
+                .foregroundStyle(.secondary)
+                .frame(width: 150, height: 150)
+            Text("Username")
+                .font(.title3)
+                .bold()
+            Text("GithubBIOen legges inn her, tekst tekst tekst teskt teskt tekst tesk")
+                .padding()
         }
         .padding()
+        
+        // Spaceren pusher alt det over til å ligge øverst på skjermen
+        Spacer()
     }
 }
 
