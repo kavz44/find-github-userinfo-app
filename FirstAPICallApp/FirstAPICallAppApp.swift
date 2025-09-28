b4889833b51931c0f696ed2389ff41079fb8918a
Kavz44 <kavingregary@outlook.com>
2025-09-28 13:28:39 +0800

    Initial Commit

---
+++ FirstAPICallApp/FirstAPICallAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAPICallAppApp.swift
+//  FirstAPICallApp
+//
+//  Created by Kavin Gregary Anand on 28/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct FirstAPICallAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
